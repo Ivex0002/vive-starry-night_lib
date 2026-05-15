53bec66995f3ee065ef36211e88dadb69b806150
ivex0002 <garam199803@gmail.com>
2026-05-15 14:59:50 +0900

    패키지 및 config 변경

---
+++ tsup.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["index.ts"],
+
+  format: ["esm", "cjs"],
+
+  dts: true,
+
+  sourcemap: true,
+
+  clean: true,
+
+  splitting: false,
+
+  external: ["react"],
+});
